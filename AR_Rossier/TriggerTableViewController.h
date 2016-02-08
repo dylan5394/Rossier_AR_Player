ab7f2e00329fbea2021583602e2a7a2cd1a2148c
hlenner <lenner@usc.edu>
2016-02-08 10:19:02 -0800

    Dylan changes on haleys computer

---
+++ AR_Rossier/TriggerTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  TriggerTableViewController.h
+//  AR_Rossier
+//
+//  Created by Haley Lenner on 2/8/16.
+//  Copyright © 2016 AR_Rossier. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TriggerTableViewController : UITableViewController
+
+@end
