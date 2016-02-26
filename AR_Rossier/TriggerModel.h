95b31446b4d0300e55e8dc5823d7f3baef78a4e2
hlenner <lenner@usc.edu>
2016-02-26 15:10:01 -0800

    Working on model stuff

--- AR_Rossier/TriggerModel.h
+++ AR_Rossier/TriggerModel.h
@@ -2,12 +2,24 @@
 //  TriggerModel.h
 //  AR_Rossier
 //
-//  Created by Dylan Kyle Davis on 2/26/16.
+//  Created by Haley Lenner on 2/26/16.
 //  Copyright © 2016 AR_Rossier. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
 
 @interface TriggerModel : NSObject
+
+- (int) value;
++ (instancetype) sharedModel;
+- (void)removeTrigger;
+- (void)addTrigger;
+- (int) numTriggers;
+- (void) updateData;
+
+//remove
+//add
+//numTriggers
+
 
 @end
